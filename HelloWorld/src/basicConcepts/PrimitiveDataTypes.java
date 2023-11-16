0710586092b13ddee1ff468329bde40a419ac061
jujcgu <juanjosecastrogutierrez@gmail.com>
2023-11-16 14:26:47 -0500

    My new branch, learning from w3schools.

---
+++ HelloWorld/src/basicConcepts/PrimitiveDataTypes.java
@@ -0,0 +1,15 @@
+package basicConcepts;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		byte myByte = 127;
+		short myShort = 32767;
+		long myLOng = 922337203;
+		float myFloat = 1.7912345678901234567890f;
+		double myDouble = 1.7912345678901234567890;
+		boolean myBool = true;
+		char myChar = 'J';
+	}
+
+}
---
+++ HelloWorld/src/basicConcepts/Variables.java
@@ -0,0 +1,16 @@
+package basicConcepts;
+
+public class Variables {
+
+	public static void main(String[] args) {		
+		String name = "Juan";
+		int age = 22;
+		float height = 1.79f;
+		char grade = 'S';
+		boolean isHandsome = true;
+		
+		System.out.println("Hello " + name);
+		
+	}
+
+}
