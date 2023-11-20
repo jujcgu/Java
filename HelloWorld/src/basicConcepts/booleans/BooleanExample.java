a600195cc4d840551b4cf2ed017f76b84eca7eb1
juancasi <juan.castro@usco.edu.co>
2023-11-20 17:31:39 -0500

    add boolean data type examples

---
+++ HelloWorld/src/basicConcepts/booleans/BooleanExample.java
@@ -0,0 +1,16 @@
+package basicConcepts.booleans;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		int myAge = 22;
+		int votingAge = 18;
+
+		if (myAge >= votingAge) {
+		  System.out.println("Old enough to vote!");
+		} else {
+		  System.out.println("Not old enough to vote.");
+		}
+	}
+
+}
---
+++ HelloWorld/src/basicConcepts/booleans/BooleanValues.java
@@ -0,0 +1,12 @@
+package basicConcepts.booleans;
+
+public class BooleanValues {
+
+	public static void main(String[] args) {
+		boolean isJavaFun = true;
+		boolean isFishTasty = false;
+		System.out.println(isJavaFun);     // Outputs true
+		System.out.println(isFishTasty);   // Outputs false
+	}
+
+}
