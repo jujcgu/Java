8adaa7c481d9e00c3393b2e8dbc3641e165e622e
juancasi <juan.castro@usco.edu.co>
2023-11-20 17:30:55 -0500

    add Math class

---
+++ HelloWorld/src/basicConcepts/JavaMath.java
@@ -0,0 +1,23 @@
+package basicConcepts;
+
+public class JavaMath {
+
+	public static void main(String[] args) {
+		// max
+		System.out.println(Math.max(5, 10));// highest 10
+
+		// min
+		System.out.println(Math.min(5, 10));// minimum 5
+
+		// square root
+		System.out.println(Math.sqrt(25));// square root of 25 is 5
+
+		// absolute
+		System.out.println(Math.abs(-45.3));// absolute positive 45.3
+
+		// random
+		System.out.println(Math.random());// random number
+
+	}
+
+}
